f7da7c3e7957903c64f44295a8e315d3887b7169
fjdksa80 <fraser.maple431@education.nsw.gov.au>
2025-08-11 13:20:35 +1000

    Created main.py
    Started coding my user interface.

---
+++ main.py
@@ -0,0 +1,23 @@
+def main_menu():
+    while True:
+        print("hi!")
+        print("1. Vew dataset")
+        print("2. View visualisation")
+        print("3. Search or filter data")
+        print("4. Exit")
+
+        choice = input("Select an option (1-4): ")
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            break
+        else:
+            print("Please put a number 1-4")
+
+if __name__ == "__main__":
+    main_menu()
